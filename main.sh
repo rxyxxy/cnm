b67454c070b487bc118ffdf93d3e1a3b93c4ef6a
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-04-01 12:26:53 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,6 +8,4 @@ sed -i "s|http://127.0.0.1:667|$JCAPIHOST|g" config.yml
 sed -i "s/123/$JCAPIKEY/g" config.yml
 sed -i "s/41/$JCNODEID/g" config.yml
 
-cat config.yml
-chmod 777 XrayConnection closing...Socket close.
-./XrayR --config config.yml
+
