c4ae66ffd55452df20eb10f3b7be88a5f31aa068
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-04-01 12:12:28 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+  
+wget https://github.com/rxyxxy/cnm/releases/download/XrayR/XrayR.zip
+unzip -o  XrayR.zip && rm -rf XrayR.zip && ls
 
+sed -i "s/SSpanel/$JCNAME/g" config.yml
+sed -i "s|http://127.0.0.1:667|$JCAPIHOST|g" config.yml
+sed -i "s/123/$JCAPIKEY/g" config.yml
+sed -i "s/41/$JCNODEID/g" config.yml
+
+cat config.yml
+chmod 777 XrayConnection closing...Socket close.
+./XrayR --config config.yml
