5d3cdfe2b80912342c0dd110506c5212146cc64a
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-04-01 15:35:02 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-  
+
+# 设置 nginx 伪装站
+rm -rf /usr/share/nginx/*
+wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
+unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
+rm -f /usr/share/nginx/mikutap.zip
+
+wget -O nginx.conf https://raw.githubusercontent.com/rxyxxy/cnm/main/nginx.conf
+mv nginx.conf /etc/nginx/nginx.conf
+service nginx start
+
+
 wget https://github.com/rxyxxy/cnm/releases/download/XrayR/XrayR.zip
 unzip -o  XrayR.zip && rm -rf XrayR.zip && ls
 
@@ -7,5 +18,3 @@ sed -i "s/SSpanel/$JCNAME/g" config.yml
 sed -i "s|http://127.0.0.1:667|$JCAPIHOST|g" config.yml
 sed -i "s/123/$JCAPIKEY/g" config.yml
 sed -i "s/41/$JCNODEID/g" config.yml
-
-
