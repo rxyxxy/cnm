c43d40b87725a0740922cacb93befa157e1ac9c1
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-04-01 15:35:49 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -11,10 +11,10 @@ mv nginx.conf /etc/nginx/nginx.conf
 service nginx start
 
 
-wget https://github.com/rxyxxy/cnm/releases/download/XrayR/XrayR.zip
-unzip -o  XrayR.zip && rm -rf XrayR.zip && ls
+# wget https://github.com/rxyxxy/cnm/releases/download/XrayR/XrayR.zip
+# unzip -o  XrayR.zip && rm -rf XrayR.zip && ls
 
-sed -i "s/SSpanel/$JCNAME/g" config.yml
-sed -i "s|http://127.0.0.1:667|$JCAPIHOST|g" config.yml
-sed -i "s/123/$JCAPIKEY/g" config.yml
-sed -i "s/41/$JCNODEID/g" config.yml
+# sed -i "s/SSpanel/$JCNAME/g" config.yml
+# sed -i "s|http://127.0.0.1:667|$JCAPIHOST|g" config.yml
+# sed -i "s/123/$JCAPIKEY/g" config.yml
+# sed -i "s/41/$JCNODEID/g" config.yml
