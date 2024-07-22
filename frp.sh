b53a1452efadd860199a2ce59c338bd2be4b6df8
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-07-22 22:12:17 +0800

    Update frp.sh

--- frp.sh
+++ frp.sh
@@ -3,7 +3,7 @@
 wget https://chmlfrp.cn/dw/ChmlFrp-0.51.2_linux_amd64.tar.gz
 tar xzf ChmlFrp-0.51.2_linux_amd64.tar.gz && rm -rf ChmlFrp-0.51.2_linux_amd64.tar.gz
 
-cd ChmlFrp-0.51.2_linux_amd64 && rm -rf frpc.ini
+cd frp_ChmlFrp-0.51.2_linux_amd64 && rm -rf frpc.ini
 
 tee frpc.ini << EOF
 [common]
